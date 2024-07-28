7c8b3deb83dfc4fa2edfa86d1d6ff5b77c62fee7
Arthur Hermitte <ahermitte@enseirb-matmeca.fr>
2024-07-28 19:22:29 +0200

    inception + div

--- .config/nvim/lua/plugins/terminal.lua
+++ .config/nvim/lua/plugins/terminal.lua
@@ -4,12 +4,18 @@ return {
   config = function()
     require("toggleterm").setup()
     local Terminal = require('toggleterm.terminal').Terminal
-    local lazygit  = Terminal:new({ cmd = "lazygit", hidden = true, direction = 'float' })
 
+    local lazygit  = Terminal:new({ cmd = "lazygit", hidden = true, direction = 'float' })
     function _lazygit_toggle()
       lazygit:toggle()
     end
+    vim.api.nvim_set_keymap("n", "<leader>tg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true, desc = "Toggle lazygit" })
 
-    vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+    local inception = Terminal:new({ cmd = "nvim", hidden = true, direction = 'float' });
+    function _inception_toggle()
+      inception:toggle()
+    end
+    vim.api.nvim_set_keymap("n", "<leader>ti", "<cmd>lua _inception_toggle()<CR>", { noremap = true, silent = true, desc = "How do I get out?" })
+
   end
 }

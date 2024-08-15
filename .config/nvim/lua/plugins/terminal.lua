633f9aca9c4fa688ada2ae89386c4eba4835faba
Arthur Hermitte <arthur.hermitte@enseirb-matmeca.fr>
2024-08-15 12:41:36 +0200

    toggle floating terminal inside nvim

--- .config/nvim/lua/plugins/terminal.lua
+++ .config/nvim/lua/plugins/terminal.lua
@@ -1,21 +1,44 @@
 return {
-  "akinsho/toggleterm.nvim",
-  tag = '*',
-  config = function()
-    require("toggleterm").setup()
-    local Terminal = require('toggleterm.terminal').Terminal
+	"akinsho/toggleterm.nvim",
+	tag = "*",
+	config = function()
+		require("toggleterm").setup()
+		local Terminal = require("toggleterm.terminal").Terminal
 
-    local lazygit  = Terminal:new({ cmd = "lazygit", hidden = true, direction = 'float' })
-    function _lazygit_toggle()
-      lazygit:toggle()
-    end
-    vim.api.nvim_set_keymap("n", "<leader>tg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true, desc = "Toggle lazygit" })
+		local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
+		function _lazygit_toggle()
+			lazygit:toggle()
+		end
 
-    local inception = Terminal:new({ cmd = "nvim", hidden = true, direction = 'float' });
-    function _inception_toggle()
-      inception:toggle()
-    end
-    vim.api.nvim_set_keymap("n", "<leader>ti", "<cmd>lua _inception_toggle()<CR>", { noremap = true, silent = true, desc = "How do I get out?" })
+		vim.api.nvim_set_keymap(
+			"n",
+			"<leader>tg",
+			"<cmd>lua _lazygit_toggle()<CR>",
+			{ noremap = true, silent = true, desc = "Toggle lazygit" }
+		)
 
-  end
+		local inception = Terminal:new({ cmd = "nvim", hidden = true, direction = "float" })
+		function _inception_toggle()
+			inception:toggle()
+		end
+
+		vim.api.nvim_set_keymap(
+			"n",
+			"<leader>ti",
+			"<cmd>lua _inception_toggle()<CR>",
+			{ noremap = true, silent = true, desc = "How do I get out?" }
+		)
+
+		local terminal = Terminal:new({ cmd = "zsh", hidden = true, direction = "float" })
+		function _terminal_toggle()
+			terminal:toggle()
+		end
+
+		vim.api.nvim_set_keymap(
+			"n",
+			"<leader>tt",
+			"<cmd>lua _terminal_toggle()<CR>",
+			{ noremap = true, silent = true, desc = "Need to do stuff?" }
+		)
+	end,
 }
